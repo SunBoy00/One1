395db5af5f4fa51ead8072548c509188f668551c
dzt <dzt@163.com>
2021-12-16 20:53:32 +0800

    second commit

--- src/main/java/com/dzt/git/TestDemo.java
+++ src/main/java/com/dzt/git/TestDemo.java
@@ -3,5 +3,6 @@ package com.dzt.git;
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println("hello,java");
+        System.out.println("hello,Scala");
     }
 }
